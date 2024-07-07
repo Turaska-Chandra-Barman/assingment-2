a5d458b06b49562d4e7e29abe7170424920ad2e2
Turaska-Chandra-Barman <barmanjatin372@gmail.com>
2024-07-08 02:54:58 +0600

    first-message

--- tailwind.config.js
+++ tailwind.config.js
@@ -55,6 +55,7 @@ module.exports = {
         '3xl':
           ' rgba(17, 17, 26, 0.05) 0px 1px 0px, rgba(17, 17, 26, 0.1) 0px 0px 8px',
       },
+      
       animation: {
         wiggle: 'wiggle 2s  ease-in-out   ',
         keyframes: {
