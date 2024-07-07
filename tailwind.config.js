c014b5427882e01b22e0f5a94e46382aff648a6e
Turaska-Chandra-Barman <barmanjatin372@gmail.com>
2024-07-07 21:29:59 +0600

    assinment-2

---
+++ tailwind.config.js
@@ -0,0 +1,64 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode:'class',
+  content: ['*.html'],
+  theme: {
+    extend: {
+      screens: {
+        sm0: '385px',
+        sm1: '420px',
+        smpro:'530px',
+        sm2: '1780px',
+        sm3: '2000px',
+      },
+      fontFamily: {
+        plus: ['Plus Jakarta Sans', 'sans-serif'],
+      },
+      maxWidth: {
+        '9xl': '580px',
+        '10xl': '1800px',
+        '11xl': '1650px',
+        '12xl': '2000px',
+        'logo-max': '350px',
+        'upskils-text':'570px',
+      },
+      height:{
+        'about-h':'680px',
+        'about-pro':'750px'
+      },
+      colors: {
+        'nav-clr': '#072125',
+        'active-clr': '#F44335',
+        'rang-clr': '#00FF84',
+        'rang-text': '#2D3142',
+        'line2-clr': '#F9CCC7',
+        'learn-text': '#181818',
+        'upskils1': '#00FF84',
+        'upskils2': '#D6D1F3',
+        'darkImg-clr':'-webkit-filter: invert(100%);'
+        
+      },
+      backgroundColor: {
+        'home-clr': '#FAFAFA',
+        'home-btn': '#20484F',
+        'line-clr': '#D1D3D4',
+      },
+     
+      fontSize: {
+        'sm1': '8px',
+        '5xl':'50px',
+        '20xl': '64px',
+        
+      },
+      textUnderlineOffset: {
+        '10px': '13px',
+      },
+      boxShadow: {
+        '3xl':
+          ' rgba(17, 17, 26, 0.05) 0px 1px 0px, rgba(17, 17, 26, 0.1) 0px 0px 8px',
+      },
+    },
+  },
+  plugins: [],
+};
+
