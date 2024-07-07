a05e00dd3f2347e5b147443bb179985537137454
Turaska-Chandra-Barman <barmanjatin372@gmail.com>
2024-07-07 22:17:55 +0600

    four-change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode:'class',
+  darkMode: 'class',
   content: ['*.html'],
   theme: {
     extend: {
       screens: {
         sm0: '385px',
         sm1: '420px',
-        smpro:'530px',
+        smpro: '530px',
         sm2: '1780px',
         sm3: '2000px',
       },
@@ -20,11 +20,11 @@ module.exports = {
         '11xl': '1650px',
         '12xl': '2000px',
         'logo-max': '350px',
-        'upskils-text':'570px',
+        'upskils-text': '570px',
       },
-      height:{
-        'about-h':'680px',
-        'about-pro':'750px'
+      height: {
+        'about-h': '680px',
+        'about-pro': '750px',
       },
       colors: {
         'nav-clr': '#072125',
@@ -33,22 +33,20 @@ module.exports = {
         'rang-text': '#2D3142',
         'line2-clr': '#F9CCC7',
         'learn-text': '#181818',
-        'upskils1': '#00FF84',
-        'upskils2': '#D6D1F3',
-        'darkImg-clr':'-webkit-filter: invert(100%);'
-        
+        upskils1: '#00FF84',
+        upskils2: '#D6D1F3',
+        'darkImg-clr': '-webkit-filter: invert(100%);',
       },
       backgroundColor: {
         'home-clr': '#FAFAFA',
         'home-btn': '#20484F',
         'line-clr': '#D1D3D4',
       },
-     
+
       fontSize: {
-        'sm1': '8px',
-        '5xl':'50px',
+        sm1: '8px',
+        '5xl': '50px',
         '20xl': '64px',
-        
       },
       textUnderlineOffset: {
         '10px': '13px',
@@ -56,6 +54,26 @@ module.exports = {
       boxShadow: {
         '3xl':
           ' rgba(17, 17, 26, 0.05) 0px 1px 0px, rgba(17, 17, 26, 0.1) 0px 0px 8px',
+      },
+      animation: {
+        wiggle: 'wiggle 2s  ease-in-out   ',
+        keyframes: {
+          wiggle: {
+            '0%': {
+              opacity: '0',
+              top: '0px',
+            },
+            '50%': {
+              opacity: '0',
+              top: '-100px',
+            },
+
+            '100%': {
+              opacity: '1',
+              top: '0px',
+            },
+          },
+        },
       },
     },
   },
